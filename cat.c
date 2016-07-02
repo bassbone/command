4398b761deabc84dba3cbbae813bc3decaa2c31d
root <root@vagrant-centos65.vagrantup.com>
2016-07-02 01:27:46 +0000

    cat

--- cat.c
+++ cat.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     FILE *fpr;
     char buf[BUFSIZ];
 
     if (argc < 2) {
         fpr = stdin;
+    } else if (strcmp(argv[argc-1],"--help") == 0) {
+        fprintf(stdout, "usage: cat [file or stdin]\n");
+        return 0;
     } else if ((fpr = fopen(argv[argc-1],"r"))==NULL) {
         fprintf(stderr, "$sというファイルが見つかりません\n", argv[argc-1]);
     } else {
