7ddd5f953a0b20c7c20002ff98658d6224757359
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-04 15:52:45 +0000

    uptime.c

--- uptime.c
+++ uptime.c
@@ -8,6 +8,9 @@ int main (int argc, char *argv[])
     char buf[BUFSIZ];
     char *loadavg_file = "/proc/loadavg";
     char *tok;
+    char la_1min[5];
+    char la_3min[5];
+    char la_5min[5];
 
     if ((fpr = fopen(loadavg_file, "r")) == NULL) {
         fprintf(stderr, "%s is not found.\n", loadavg_file);
@@ -15,13 +18,20 @@ int main (int argc, char *argv[])
     }
 
     if (fgets(buf, sizeof(buf), fpr) != NULL) {
-        fprintf(stdout, "load average: ");
         tok = strtok(buf, " ");
-        fprintf(stdout, "1min %s, ", tok);
+        strcpy(la_1min, tok);
         tok = strtok(NULL, " ");
-        fprintf(stdout, "3min %s, ", tok);
+        strcpy(la_3min, tok);
         tok = strtok(NULL, " ");
-        fprintf(stdout, "5min %s\n", tok);
+        strcpy(la_5min, tok);
+
+        if (strcmp(argv[argc - 1], "--csv") == 0) {
+            fprintf(stdout, "%s,%s,%s\n", la_1min, la_3min, la_5min);
+        } else if (strcmp(argv[argc - 1], "--json") == 0) {
+            fprintf(stdout, "{\"1min\":\"%s\",\"3min\":\"%s\",\"5min\":\"%s\"}\n", la_1min, la_3min, la_5min);
+        } else {
+            fprintf(stdout, "load average: 1min %s, 3min %s, 5min %s\n", la_1min, la_3min, la_5min);
+        }
     }
 }
 
